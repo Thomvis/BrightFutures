54452429eb1f83a13f6a872c3129bc1c045e6cfd
Thomas Visser <thomas.visser@gmail.com>
2014-06-08 12:18:47 +0200

    Initial Commit

---
+++ BrightFutures/BrightFutures.h
@@ -0,0 +1,19 @@
+//
+//  BrightFutures.h
+//  BrightFutures
+//
+//  Created by Thomas Visser on 08/06/14.
+//  Copyright (c) 2014 Thomas Visser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BrightFutures.
+FOUNDATION_EXPORT double BrightFuturesVersionNumber;
+
+//! Project version string for BrightFutures.
+FOUNDATION_EXPORT const unsigned char BrightFuturesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BrightFutures/PublicHeader.h>
+
+
