4a4bfabb6de40ddacfdccea5ba36c56da8e40e92
Thomas Visser <thomas.visser@gmail.com>
2014-08-03 00:16:13 +0200

    improved the traverse implementation, now powered by fold

--- BrightFutures/FutureUtils.swift
+++ BrightFutures/FutureUtils.swift
@@ -28,38 +28,13 @@ public class FutureUtils {
     }
     
     public class func traverse<S : Sequence,T, U where S.GeneratorType.Element == T>(seq: S, context c: ExecutionContext, fn: T -> Future<U>) -> Future<[U]> {
-        let futureSequence = map(seq, fn)
-
-        let p = Promise<[U]>()
-        var resultingArray = [U]()
         
-        var baseFuture = Future<Void>.succeeded()
-        for future in futureSequence {
-            let p1 = Promise<Void>()
-            baseFuture = baseFuture.andThen { _ in
-                future.onComplete(context: c) { res in
-                    switch res {
-                    case .Success(let val):
-                        resultingArray.append(val)
-                        p1.success()
-                    case .Failure(let err):
-                        p.tryError(err)
-                    }
-                }
-                return
-            }
-            baseFuture = p1.future
-        }
-        
-        baseFuture.onSuccess { _ in
-            p.success(resultingArray)
-        }
-        
-        baseFuture.onFailure { err in
-            p.error(err)
-        }
-        
-        return p.future
+        return self.fold(map(seq, fn), context: c, zero: [U](), op: { (list: [U], elem: U) -> [U] in
+            // this should be even nicer in beta 5
+            var l = list
+            l += elem
+            return l
+        })
     }
     
 }
