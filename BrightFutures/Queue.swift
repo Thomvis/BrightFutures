9eccad82123d60fb4c895733fb88bd84376ac7fb
Thomas Visser <thomas.visser@gmail.com>
2015-10-05 15:56:41 +0200

    fixes crash when creating a Queue

--- BrightFutures/Queue.swift
+++ BrightFutures/Queue.swift
@@ -58,7 +58,7 @@ public struct Queue {
         self.init(queueLabel: "queue")
     }
     
-    public init(queueLabel: String, attributes: dispatch_queue_attr_t = DISPATCH_QUEUE_SERIAL) {
+    public init(queueLabel: String, attributes: dispatch_queue_attr_t! = DISPATCH_QUEUE_SERIAL) {
         self.init(queue: dispatch_queue_create(queueLabel, attributes))
     }
     
