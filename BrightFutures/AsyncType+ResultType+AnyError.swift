ef47a05a4f7d4c73ee4bfe5836ac12d67ad05050
Daniel Leping <dileping@gmail.com>
2015-12-22 21:08:31 +0200

    Added some extensions to standard futures

--- BrightFutures/AsyncType+ResultType+AnyError.swift
+++ BrightFutures/AsyncType+ResultType+AnyError.swift
@@ -10,16 +10,19 @@ import Foundation
 import Result
 
 /// Executes the given task on `Queue.global` and wraps the result of the task in a Future
+/// If the given closure throws, returns an error
 public func future<T>(@autoclosure(escaping) task: () throws -> T) -> Future<T, AnyError> {
     return future(Queue.global.context, task: task)
 }
 
 /// Executes the given task on `Queue.global` and wraps the result of the task in a Future
+/// If the given closure throws, returns an error
 public func future<T>(task: () throws -> T) -> Future<T, AnyError> {
     return future(Queue.global.context, task: task)
 }
 
 /// Executes the given task on the given context and wraps the result of the task in a Future
+/// If the given closure throws, returns an error
 public func future<T>(context: ExecutionContext, task: () throws -> T) -> Future<T, AnyError> {
     return future(context: context) { () -> Result<T, AnyError> in
         do {
@@ -102,6 +105,42 @@ public extension AsyncType where Value: ResultType, Value.Error == AnyError {
     public func mapError<E1: ErrorType>(context: ExecutionContext, f: ErrorType -> E1) -> Future<Value.Value, E1> {
         return mapError(context) { (error:Value.Error) -> E1 in
             f(error.cause)
+        }
+    }
+}
+
+public extension AsyncType where Value: ResultType {
+    /// See `map<U>(context c: ExecutionContext, f: (T) -> U) -> Future<U>`
+    ///
+    /// If the given closure throws, returns an error
+    /// For now method name is temporary tryMap, because swift compiler can not recognise throwing closures sometimes (temporary solution)
+    /// The given closure is executed according to the default threading model (see README.md)
+    public func map<U>(f: Value.Value throws -> U) -> Future<U, AnyError> {
+        return self.map(DefaultThreadingModel(), f: f)
+    }
+    
+    /// Returns a future that succeeds with the value returned from the given closure when it is invoked with the success value
+    /// from this future. If this future fails, the returned future fails with the same error.
+    /// If the given closure throws, returns an error
+    /// For now method name is temporary tryMap, because swift compiler can not recognise throwing closures sometimes (temporary solution)
+    /// The closure is executed on the given context. If no context is given, the behavior is defined by the default threading model (see README.md)
+    public func map<U>(context: ExecutionContext, f: Value.Value throws -> U) -> Future<U, AnyError> {
+        return self.mapError(context) { e in
+            AnyError(cause: e)
+        }.map(f)
+    }
+    
+    /// Returns a future that completes with this future if this future succeeds or with the value returned from the given closure
+    /// when it is invoked with the error that this future failed with.
+    /// For now method name is temporary tryRecover, because swift compiler can not recognise throwing closures sometimes (temporary solution)
+    /// The closure is executed on the given context. If no context is given, the behavior is defined by the default threading model (see README.md)
+    public func recover(context c: ExecutionContext = DefaultThreadingModel(), task: Value.Error throws -> Value.Value) -> Future<Value.Value, AnyError> {
+        return self.recoverWith(context: c) { error -> Future<Value.Value, AnyError> in
+            do {
+                return Future<Value.Value, AnyError>(value: try task(error))
+            } catch let e {
+                return Future<Value.Value, AnyError>(error: AnyError(cause: e))
+            }
         }
     }
 }
