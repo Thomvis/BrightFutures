d153aaba5a310b3c99a0344f553e714e95e45477
Oleksii Dykan <alick_bass@mail.ru>
2016-09-23 11:30:49 +0200

    Cover String lastPathComponent with unit tests
    This test input are taken from https://developer.apple.com/reference/foundation/nsstring/1416528-lastpathcomponent

--- BrightFuturesTests/FutureDebugTests.swift
+++ BrightFuturesTests/FutureDebugTests.swift
@@ -7,29 +7,16 @@
 //
 
 import XCTest
+@testable import BrightFutures
 
 class FutureDebugTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testStringLastPathComponent() {
+        XCTAssertEqual("/tmp/scratch.tiff".lastPathComponent, "scratch.tiff")
+        XCTAssertEqual("/tmp/scratch".lastPathComponent, "scratch")
+        XCTAssertEqual("/tmp/".lastPathComponent, "tmp")
+        XCTAssertEqual("scratch///".lastPathComponent, "scratch")
+        XCTAssertEqual("/".lastPathComponent, "/")
     }
     
 }
