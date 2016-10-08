d98976947d3212e497726941c4f34eeca1b65c36
Ward van Teijlingen <ward@moonbase.co>
2016-10-09 00:11:45 +0200

    Added support for Swift Package Manager.

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "BrightFutures",
+    dependencies: [
+        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3),
+    ]
+)
