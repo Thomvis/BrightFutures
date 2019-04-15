46b18f423ea3969569d428a48f40f47a5b021504
Scott Lessans <slessans@gmail.com>
2019-04-15 15:41:56 -0700

    Updated Package.swift to work with swift 5

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-    name: "BrightFutures"
+    name: "BrightFutures",
+    products: [
+        .library(
+            name: "BrightFutures",
+            targets: ["BrightFutures"]),
+    ],
+    targets: [
+        .target(
+            name: "BrightFutures",
+            dependencies: []),
+    ]
 )
